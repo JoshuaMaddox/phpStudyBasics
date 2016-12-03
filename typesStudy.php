89406e65bfe6b82016ffbbcaf1c86a4d266b1019
Joshua Maddox <joshua@cinderhousecreative.com>
2016-12-03 14:48:31 +0800

    first commit

---
+++ typesStudy.php
@@ -0,0 +1,12 @@
+<?php 
+
+define("A_Constant", "The Constant's Value"); 
+
+$iAmAnInt = 768;
+$iAmAString = "I am the string's value";
+$iAmAFloat = 34.4562;
+$iAmABool = true; 
+
+is_Int($iAmAnInt);
+
+?> 
